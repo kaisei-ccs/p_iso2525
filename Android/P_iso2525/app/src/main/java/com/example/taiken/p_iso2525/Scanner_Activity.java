ecd9ba7824b0883c894424d0f868510997c19afa
hira <Piso2525@users.noreply.github.com>
2016-10-21 14:25:14 +0900

    Android(プロジェクト)

---
+++ Android/P_iso2525/app/src/main/java/com/example/taiken/p_iso2525/Scanner_Activity.java
@@ -0,0 +1,13 @@
+package com.example.taiken.p_iso2525;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Scanner_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner_);
+    }
+}
