651c2422e4c91d3624e660b640491e27cf1df77b
kbut1 <kbut1@users.noreply.github.com>
2016-10-23 13:46:38 +0900

    DBInterfaceに接続処理追加

--- Web/src/common/DB_Interface.java
+++ Web/src/common/DB_Interface.java
@@ -1,5 +1,66 @@
 package common;
 
-public class DB_Interface {
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+
+public class DB_Interface {
+	//singletonパターン用
+	private static DB_Interface DBI = new DB_Interface();
+
+	//DB
+	private Connection con;
+	
+	private DB_Interface(){
+		String dbName = null;
+		String dbUser = null;
+		String dbPass = null;
+
+		//プロパティから接続DB情報取得
+		InputStream is = getClass().getClassLoader().getResourceAsStream("db.properties");
+		Properties prop = new Properties();
+		try{
+			prop.load(is);
+			dbName = prop.getProperty("db.name");
+			dbUser = prop.getProperty("db.user");
+			dbPass = prop.getProperty("db.pass");
+		}catch (IOException e){
+			e.printStackTrace();
+		}finally{
+			if(is != null){
+				try{
+					is.close();
+				}catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+
+		
+		//SQLServerに接続
+		try{
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			con = DriverManager.getConnection(
+					"jdbc:sqlserver://localhost\\SQLEXPRESS;database="+ dbName +";",
+					dbUser,dbPass);
+		}catch (SQLException e){
+			e.printStackTrace();
+		}catch (ClassNotFoundException e){
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static DB_Interface getInstance(){
+		return DBI;
+	}
+	
+	public Connection getConnection(){
+		return con;
+	}
+	
 }
