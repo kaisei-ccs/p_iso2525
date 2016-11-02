6f8bd236853a0ad99ff7eaf39739251fbf1358e4
orzwaesrd <orzwaesrd@users.noreply.github.com>
2016-11-02 15:38:46 +0900

    Item.javaを変更

--- Web/src/model/Item.java
+++ Web/src/model/Item.java
@@ -259,7 +259,12 @@ public class Item extends BaseActiveRecord{
 		return executeSelectQuery("select * from ITEM where PRICE=" + price);
 	}
 	public static ArrayList<Item> findByPrintFlg(boolean printFlg){
-		return executeSelectQuery("select * from ITEM where PRINTFLG=" + printFlg);
+		int flg = 0;
+		if(true == printFlg){
+			flg = 1;
+		}
+		return executeSelectQuery("select * from ITEM where PRINTFLG=" + flg);
+		//return executeSelectQuery("select * from ITEM where PRINTFLG = false");
 	}
 	public static ArrayList<Item> findByBarcodeData(int esID,int iNO){
 		return executeSelectQuery("select * from ITEM where es_ID=" + esID + "AND I_NO =" + iNO);
