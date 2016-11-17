51006c0b5641e29031a66208ba1157201cd2e68b
deru01 <deru01@users.noreply.github.com>
2016-11-17 12:19:10 +0900

    個人別精算表変更

--- Web/src/view/Print_Sell_Result.java
+++ Web/src/view/Print_Sell_Result.java
@@ -22,19 +22,15 @@ public class Print_Sell_Result extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     private ArrayList<Seller>SellerList = new ArrayList<Seller>();
     private ArrayList<ArrayList<Item>>ItemList = new ArrayList<ArrayList<Item>>();
-    private ArrayList<Integer>SumList = new ArrayList<Integer>();
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		/*int size = Integer.parseInt(request.getParameter("size"));
-		for(int i=0; i<size; i++){
-			SellerList.add(Seller.findBySID(Integer.parseInt(request.getParameter("sid"+i))).get(0));
-		}*/
-		int size = 1;
-		for(int i=0; i<size; i++){
-			SellerList.add(Seller.findBySID(100).get(0));
+		String sellerId[ ] = request.getParameterValues("selId");
+		for(int i=0; i<sellerId.length; i++){
+			int id = Integer.parseInt(sellerId[i]);
+			SellerList.add(Seller.findBySID(id).get(0));
 		}
 		for(int i=0; i<SellerList.size(); i++){
 			ArrayList<Item> items = new ArrayList<Item>();
@@ -49,12 +45,10 @@ public class Print_Sell_Result extends HttpServlet {
 		}
 		request.setAttribute("SellerList", SellerList);
 		request.setAttribute("ItemList", ItemList);
-		request.setAttribute("SumList", SumList);
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/Print/Sell_Result.jsp");
 		dispatcher.forward(request, response);
 		SellerList.clear();
 		ItemList.clear();
-		SumList.clear();
 	}
 
 	/**
